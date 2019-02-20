4a4495742c7c63fed5c0e1d873fafe2b5ababdc3
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-20 18:33:01 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,9 +8,10 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.songs.count
   end
 end
